5eea555d0ba89145ef407e9b69879e49123445f0
s1hiro <hirodoeswork@gmail.com>
2025-07-25 14:40:08 -0400

    upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const toggleButton = document.getElementById('toggleButton');
+// const sideNav = document.getElementById('sideNav');
+// const mainContent = document.querySelector('.main-content');
+
+// toggleButton.addEventListener('click', () => {
+//     sideNav.classList.toggle('open');
+//     toggleButton.classList.toggle('open'); 
+
+//     if (sideNav.classList.contains('open')) {
+//         sideNav.style.width = '250px';
+//         mainContent.style.marginLeft = '250px'; 
+//     } else {
+//         sideNav.style.width = '0';
+//         mainContent.style.marginLeft = '0'; 
+//     }
+// });
